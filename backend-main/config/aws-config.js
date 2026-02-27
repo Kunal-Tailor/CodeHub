9950fae903a39c538aeb18f86fbd0a764901183e
Kunal Tailor <kunaltailor5555@gmail.com>
2026-02-27 19:11:00 +0530

    first commit

--- backend-main/config/aws-config.js
+++ backend-main/config/aws-config.js
@@ -8,19 +8,4 @@ const S3_BUCKET = "samplekunalbucket";
 module.exports = { s3, S3_BUCKET };
 
 
-// {
-//   "Version": "2012-10-17",
-//   "Statement": [
-//     {
-//       "Effect": "Allow",
-//       "Principal": {
-//         "AWS": "arn:aws:iam::043673801062:user/demouser"
-//       },
-//       "Action": "s3:*",
-//       "Resource": [
-//         "arn:aws:s3:::samplekunalbucket",
-//         "arn:aws:s3:::samplekunalbucket/*"
-//       ]
-//     }
-//   ]
-// }
+
